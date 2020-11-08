983dd1a4be58f576bfc519e2bc7e78ddbf717b9f
DionatanLeao <dionatan.ti@outlook.com>
2020-11-08 20:03:07 -0300

    Melhorias no layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatInputModule } from '@angular/material/input';
-import { HttpClientModule } from '@angular/common/http';
-import { ReactiveFormsModule } from '@angular/forms'
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatTableModule } from '@angular/material/table';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -25,6 +29,10 @@ import { ContatoService } from './contato.service';
     MatInputModule,
     HttpClientModule,
     ReactiveFormsModule,
+    MatTabsModule,
+    MatTableModule,
+    MatIconModule,
+    MatCardModule,
     BrowserAnimationsModule
   ],
   providers: [
